a117b91eb8033be5d623f45dc55e20b802222dc2
Julius van Dis <vandis.j@gmail.com>
2013-04-03 00:04:03 +0200

    Set-up of git repo
    Signed-off-by: Julius van Dis <vandis.j@gmail.com>

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+     	
+	<!-- set important metadata -->
+	<title>Homemade Water</title>
+
+	<!-- the description, is the description people will find on GOOGLE results -->
+	<meta name="description" content="Homemade Water is een frisse pop-rock band die elk feestzaal kan veranderen in een zaal vol dansende, springende en zingende mensen." />
+	
+	<meta name="author" content="Homemade Water" />
+	<meta name="keywords" content="Homemade Water, band, coverband, Laurens Mensink, Andrea Forzoni, Eline Burger, Moos Meijer, Julius van Dis" />
+
+	<link href="images/shortcut.jpg" type="image/jpg" rel="shortcut icon" />
+	
+	<link rel="stylesheet" type="text/css" href="style/main.css" title="style" />	
+</head>
+
+<body>
+ <div id="wrapper">
+  <!-- show the logo -->
+  <div id="logo">
+	<img src="images/logo - Copy.jpg" alt="Homemade Water logo"/>
+  </div>
+  
+  		       
+        <div id="head-content"> 
+         <h1> Welkom op de website van de Nederlandse pop/rock cover-band Homemade Water </h1>
+         <h2 class="Story">	Momenteel zijn wij druk bezig met de constructie van onze gloednieuwe website. Om op de hoogte te blijven van het laatste nieuws verwijzen wij je naar onze <a href="http://www.facebook.com/HomemadeWater">Facebook</a> pagina. Vergeet ook niet om eens onze muziek te beluisteren op <a href="http://www.soundcloud.com/Homemade-Water">Soundcloud</a></h2>
+        	<div id="signed">
+        		<h2> Homemade Water: <br /> Laurens Mensink, Eline Burger, Andrea Forzoni, Moos Meijer &amp Julius van Dis </h2> 	
+        	</div>
+        </div>
+      	
+    	<div id="social-media-icons">
+          <p class="scIcon"><a href="http://www.facebook.com/HomemadeWater"><img src="images/Facebook homemade water.png" alt="Facebook"/></a></p>
+          <p class="scIcon"><a href="https://soundcloud.com/homemade-water"><img src="images/Soundcloud homemade water.png" alt="Soundcloud"/></a></p>
+          <p class="scIcon"><a href="http://www.youtube.com/user/HomemadeWaterOfficia"><img src="images/Youtube homemade water.png" alt="Youtube"/></a></p>
+        </div>
+        
+        
+         <div id="contact">
+             <span><a href="mailto:info@homemadewater.nl">
+                <p> Neem  contact  met ons op!  </p>
+             </a></span>
+         </div>  
+    
+   <div id="keywords">
+   		Keywords: Homemade Water, band, music, cover, coverband, dutch, nederlands, Laurens Mensink, Andrea Forzoni, Eline Burger, Moos Meijer, Julius van Dis
+   </div>  
+   
+ </div>	
+
+</body>
+
+</html>
