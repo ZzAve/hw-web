e84666c41316ebf617d537e9bf31b027523c85af
Julius van Dis <vandis.j@gmail.com>
2013-05-30 13:09:25 +0200

    hoofdletterfout gefixt
    er stond .PNG ipv .png (hoofdeletters). Daardoor snapte de site het niet
    helemaal.. Maar gefixt, en 1e demo versie is online

--- development/index.php
+++ development/index.php
@@ -17,7 +17,11 @@
 
 	<div id="content"> 
          <h1> Welkom op de site van Homemade Water</h1>
-         <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum.</p>
+         <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. </p>
+                  <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. </p>
+                           <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. </p>
+                                    <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. </p>
+                                             <p> De hippe, frisse en vooral toffe pop/rock-coverband die maar wat een feestje maakt! En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. En dan nog wat Lorum Ipsum. </p>
          <img alt:"awesome picture" src="images/logo.jpg" />    
     </div>
 
