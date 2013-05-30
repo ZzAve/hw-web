0d2af9055966346a7afe149f781bb2d138fecf61
emburger <elinebrgr@gmail.com>
2013-05-30 19:08:19 +0200

    resolved  conflicts\

--- development/contact.php
+++ development/contact.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style/main.css" title="style" />
-<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 
+<!--  Links  -->  
+<link rel="stylesheet" type="text/css" href="style/main.css" title="style" />
+<link rel="stylesheet" type="text/css" href="style/contact.css" title="style" />
+
+<!--  Scripts  --> 
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="./js/validate.js"></script>
 
-<title>Untitled Document</title>
+
+<title>contactformulier</title>
+
 </head>
+
 
 <body>
 
@@ -75,7 +83,7 @@
     </form>
     
     
-    </div> <!-- end div middle -->
+    </div> <!-- end div content -->
     
     <?php include 'footer.html' ?>
 </div> <!-- end content-bar -->
