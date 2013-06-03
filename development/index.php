1e429f3e4e08bc4c6f0c5720743ef468cea7d9c7
emburger <elinebrgr@gmail.com>
2013-06-03 18:17:22 +0200

    merge opgelost

--- development/index.php
+++ development/index.php
@@ -24,7 +24,9 @@
 <div id="content-bar">
 	<div id="content"> 
          <h1> Welkom op de site van Homemade Water!</h1>
+
          <h3> Homemade Water is een frisse pop-rock(cover)band die naam begint te krijgen Delft en de rest van Nederland. Homemade Water brengt een strakke en gevarieerde set bekendere rock- en popnummers en enkele eigen nummers. Met deze set zijn al vele zalen, café's en sociëteiten op de kop gezet waarbij de temperatuur regelmatig boven het kookpunt is uitgestegen!</h3>            
+
          
        <!--  <p> Op onze site kunt u terecht voor al het moois dat Homemade Water de wereld te bieden heeft. Zo kunt u ons laatste nieuws bekijken, onze unieke sound beluisteren, maar u kunt een aantal optreden herleven met uniek beeld- en videomateriaal. Mocht u contact willen opnemen, schroom dan vooral niet.</p>-->
 
--- development/nieuws.php
+++ development/nieuws.php
@@ -1,15 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="style/main.css" title="style" />
-<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
-<link rel="stylesheet" type="text/css" href="style/nieuws.css" title="style" />
-<script type="text/javascript" src="./js/validate.js"></script>
 
-<title>Untitled Document</title>
-</head>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- <base href="www.homemadewater.nl/development" target="_blank" /> -->
 <link href="images/shortIcon.jpg" rel="shortcut icon" />
@@ -18,42 +10,61 @@
 <meta name="author" content="Homemade Water">
 <meta name="publisher" content="Homemade Water" />
 <meta name="Homemade Water" content="Delft band cover coverband Laurens Mensink Eline Burger Moos Meijer Andrea Forzoni Julius van Dis" />
-<title>Homemade Water</title>
+
+<title>Homemade Water - Nieuws</title>
+<link rel="stylesheet" type="text/css" href="style/main.css" title="style" />
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+<link rel="stylesheet" type="text/css" href="style/nieuws.css" title="style" />
+<script type="text/javascript" src="./js/validate.js"></script>
 <body>
 <?php include 'header.html'; ?>
 <div id="content-bar">
 
     <div id="content">
-		<h1> Blijf op de hoogte van Homemade Water's laatste nieuws !</h1> 
+		<h1> Blijf op de hoogte van Homemade Water's laatste nieuws!</h1> 
         <h4> Om ons op de voet te volgen, verwijzen we u door naar onze <a href="http://www.facebook.com/HomemadeWater">facebook pagina</a></h4>   <br />
         
        <div class="newsitem">
             <div class="date"><label> 3 </label><span> JUN</span></div>
         	    <h2>  Laurens' deelname aan de Beste Singer-Songwriter van Nederland</h2>
             	<img src="images/albums/laurens_nieuws_thumb.jpg" alt="item pic" title="item title" />
-                <p> Misschien hebben jullie het al voorbij zien komen op Facebook of op tv en site van het programma, maar onze lead-singer Laurens heeft het weten te schoppen tot de tv-show van de Beste Singer-Songwriter van Nederland! Om daar te komen heeft hij zich een weg moeten banen door de meer dan 600 inschrijvingen heen om zo te belanden bij de laatste 32 kandidaten waarmee het programma begint. Hoe hij het daar gaat schoppen kunnen jullie maandagavond 3 juni zien op Nederland 3 om 20.25! Kijken dus! </p>
+                <p> Misschien hebben jullie het al voorbij zien komen op Facebook of op tv en site van het programma, maar onze lead-singer Laurens heeft het weten te schoppen tot de tv-show van de Beste Singer-Songwriter van Nederland! Om daar te komen heeft hij zich een weg moeten banen door de meer dan 600 inschrijvingen heen om zo te belanden bij de laatste 32 kandidaten waarmee het programma begint. Hoe hij het daar gaat schoppen kunnen jullie <strong>maandagavond 3 juni zien op Nederland 3 om 20.25!</strong> Kijken dus! </p>
        </div>
         
         <div class="newsitem">
             <div class="date"><label> 3 </label><span> JUN</span></div>
         	    <h2>  HomemadeWater.nl officieel online!</h2>
             	<img src="images/apenstaartje_voor_nieuws.jpg" alt="item pic" title="item title" />
-                <p> Jaaaaa mensen, het is zover! HomemadeWater.nl is nu officieel online! Het feit dat dit te lezen is, geeft voldoende bewijs. Met deze site houden we jullie op de hoogte van onze gigs, nieuwtjes en ontwikkelingen! Bovendien zijn er foto's, video's en audio opnamen te vinden van onze optredens en nummers. Ook kunt u via deze site gemakkelijk contact met ons opnemen voor boekingen of vragen. </p>
+                <p> Jaaaaa mensen, het is zover! HomemadeWater.nl is nu officieel online! Het feit dat dit te lezen is, geeft voldoende bewijs. Met deze site houden we jullie op de hoogte van onze gigs, nieuwtjes en ontwikkelingen!</p>
+                <p> Bovendien zijn er foto's, video's en audio opnamen te vinden van onze optredens en nummers. Ook kunt u via deze site gemakkelijk contact met ons opnemen voor boekingen of vragen. </p>
        </div>
         
         <div class="newsitem">
             <div class="date"><label> 4 </label><span> MEI</span></div>
             	<h2>  Voorronde van The Clash of the Coverbands </h2>
+<<<<<<< HEAD
             	<img src="images/albums/1e ronde Clash of the Coverbands @ P3_thumb.jpg" alt="item pic" title="item title" />
                 <p> Na onze inschrijving bij The Clash of the Coverbands, een Benelux competitie voor coverbands, kregen we te horen dat we uit 1500 bands waren geselecteerd om deel te nemen aan de regionale voorronde bij Poppodium P3 in Purmerend! Dus hop, daar gingen we met z'n allen en onze instrumenten met het openbaar vervoer. Onze eerste tegenstanders waren wel aardig, maar na het optreden van de sterke band 'de Ideale Schoonzoons', die een grote fan share hadden meegenomen, moesten we toch even slikken. Zelf hadden we helaas helemaal geen supporters meegenomen maar het lukte ons om de supporters van onze concurrerende band los te laten gaan met een geslaagd optreden! Voor dit optreden werden we beloond met de jury prijs en bekroond tot "beste band van de avond"! Het uitgebreide jury verslag valt <a href="http://www.theclashofthecoverbands.com/show/verslagen/item,671,Voorronde-Regio-West-2013.html"> HIER</a> te lezen.
 De volgende ronde vindt plaats ergens in September in het Paard van Troje in Den Haag (iets dichter bij huis)! Nog (maar) 150 bands te verslaan en we worden de beste band van de Benelux ;)! </p>
+=======
+            	<img src="images/albums/1e ronde Clash of the Coverbands @ P3/Clash_Purmerend_1.jpg" alt="Foto voorronde Clash" title="Homemade Water is door de voorrondes van Clash of the Coverbands!" />
+                <p> Nadat we ons hadden ingeschreven voor The Clash of the Coverbands, een Benelux competitie voor coverbands, kregen we te horen dat we uit 1500 bands waren geselecteerd om deel te nemen aan de regionale voorronde bij Poppodium P3 in Purmerend! Dus hop, daar gingen we met z'n allen en onze instrumenten met het openbaar vervoer. </p>
+                <p>Onze eerste tegenstanders waren wel aardig, maar na het optreden van de sterke band 'de Ideale Schoonzoons', die een grote fan share hadden meegenomen, moesten we toch even slikken. Zelf hadden we helaas helemaal geen supporters meegenomen maar het lukte ons om de supporters van onze concurrerende band los te laten gaan met een geslaagd optreden! </p>
+                <p>Voor dit optreden werden we beloond met de jury prijs en bekroond tot "beste band van de avond"! Het uitgebreide jury verslag valt <a href="http://www.theclashofthecoverbands.com/show/verslagen/item,671,Voorronde-Regio-West-2013.html"> HIER</a> te lezen.</p>
+
+				<p>De volgende ronde vindt plaats ergens in September in het Paard van Troje in Den Haag (iets dichter bij huis)! Nog (maar) 150 bands te verslaan en we worden de beste band van de Benelux ;)! </p>
+>>>>>>> 8b6622aa689b96a8ef9abdde70a331b2c7c54685
         </div>
         
        	<div class="newsitem">
             <div class="date"><label> 25 </label><span> APRIL</span></div>
         	    <h2>  Optreden  bij windsurf vereniging Plankenkoorts</h2>
-            	<img src="images/albums/Plankenkoorts @ Floratheater_nieuws.jpg" alt="item pic" title="item title" />
-                <p> 25 April traden we op bij een feest genaamd Overstag in het Floratheater in onze mooie thuisstad: Delft. Het Floratheater bleek mooi oud pornotheater compleet met houten bioscoop stoeltjes te zijn. Gelukkig was het vroegere publiek thuis gebleven en stond de zaal nu vol met windsurf dudes en chicks van de delfste studentenvereniging Plankenkoorts. Het publiek was al snel in beweging gebracht en bij het nog redelijk relaxte nummer 'New Shoes' van Paolo Nutini ontstond er al een mosh-pit. De sfeer zat er goed in en de temperatuur nam snel toe! Na afloop van het optreden werd er hard geroepen om NOG EEN NUMMER! En tsja..wij zijn de slechtsten niet, dus hebben we ze nog een toegift cadeau gedaan. Dj's zoals "dj Pilskabouter" namen het publiek van ons over en wij mengden ons in het publiek om van het geslaagde feestje te genieten. </p>
+            	<img src="images/nieuws_plankenkoorts.jpg" alt="Plankenkoorts_foto" title="Het optreden bij Plankenkoorts" />
+                <p> 25 April traden we op bij een feest genaamd Overstag in het Floratheater in onze mooie thuisstad: Delft. Het Floratheater bleek een mooi oud pornotheater compleet met houten bioscoop stoeltjes te zijn. </p>
+                <p>Gelukkig was het vroegere publiek thuis gebleven en stond de zaal nu vol met windsurf dudes en chicks van de Delfste studentenvereniging Plankenkoorts. Het publiek was al snel in beweging gebracht en bij het nog redelijk relaxte nummer 'New Shoes' van Paolo Nutini ontstond er al een mosh-pit. </p>
+                
+                <p>De sfeer zat er goed in en de temperatuur nam snel toe! Na afloop van het optreden werd er hard geroepen om NOG EEN NUMMER! En tsja.. wij zijn de slechtsten niet, dus hebben we ze nog een toegift cadeau gedaan. Dj's zoals "DJ Pilskabouter" namen het publiek van ons over en wij mengden ons in het publiek om van het geslaagde feestje te genieten. </p>
        </div>
            
     </div> <!-- end div content -->
