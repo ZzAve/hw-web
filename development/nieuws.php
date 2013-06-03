420fb6b2c038bfa74070d28d528ecfa285957db6
emburger <elinebrgr@gmail.com>
2013-06-03 15:26:38 +0200

    nieuwspagina up to date

--- development/nieuws.php
+++ development/nieuws.php
@@ -16,20 +16,36 @@
 <div id="content-bar">
 
     <div id="content">
-		<h1> Aanschouw hier het laatste nieuws omtrent het reilen en zeilen van Homemade Water</h1> 
-        <h6> Om op de hoogte te blijven van ons laatste nieuws verwijzen wij u ook door naar onze <a href="www.facebook.com/HomemadeWater">facebook pagina</a></h6>       
+		<h1> Blijf op de hoogte van Homemade Water's laatste nieuws !</h1> 
+        <h4> Om ons op de voet te volgen, verwijzen we u door naar onze <a href="http://www.facebook.com/HomemadeWater">facebook pagina</a></h4>   <br />
+        
+       <div class="newsitem">
+            <div class="date"><label> 3 </label><span> JUN</span></div>
+        	    <h2>  Laurens' deelname aan de Beste Singer-Songwriter van Nederland</h2>
+            	<img src="images/albums/laurens_nieuws_thumb.jpg" alt="item pic" title="item title" />
+                <p> Misschien hebben jullie het al voorbij zien komen op Facebook of op tv en site van het programma, maar onze lead-singer Laurens heeft het weten te schoppen tot de tv-show van de Beste Singer-Songwriter van Nederland! Om daar te komen heeft hij zich een weg moeten banen door de meer dan 600 inschrijvingen heen om zo te belanden bij de laatste 32 kandidaten waarmee het programma begint. Hoe hij het daar gaat schoppen kunnen jullie maandagavond 3 juni zien op Nederland 3 om 20.25! Kijken dus! </p>
+       </div>
+        
         <div class="newsitem">
             <div class="date"><label> 3 </label><span> JUN</span></div>
-            	<h2>  Laurens neemt deel aan DBSSW!</h2>
-            	<img src="./images/logo.jpg" alt="item pic" title="item title" />
-                <p> Trots als een pauw mag Laurens meedoen aan de beste singer- songwriter van Nederland. Vet Leuk. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. v</p>
+        	    <h2>  HomemadeWater.nl officieel online!</h2>
+            	<img src="images/apenstaartje_voor_nieuws.jpg" alt="item pic" title="item title" />
+                <p> Jaaaaa mensen, het is zover! HomemadeWater.nl is nu officieel online! Het feit dat dit te lezen is, geeft voldoende bewijs. Met deze site houden we jullie op de hoogte van onze gigs, nieuwtjes en ontwikkelingen! Bovendien zijn er foto's, video's en audio opnamen te vinden van onze optredens en nummers. Ook kunt u via deze site gemakkelijk contact met ons opnemen voor boekingen of vragen. </p>
+       </div>
+        
+        <div class="newsitem">
+            <div class="date"><label> 4 </label><span> MEI</span></div>
+            	<h2>  Voorronde van The Clash of the Coverbands </h2>
+            	<img src="images/albums/1e ronde Clash of the Coverbands @ P3/901027_212162885574412_403616290_o.jpg" alt="item pic" title="item title" />
+                <p> Nadat we ons hadden ingeschreven voor The Clash of the Coverbands, een Benelux competitie voor coverbands, kregen we te horen dat we uit 1500 bands waren geselecteerd om deel te nemen aan de regionale voorronde bij Poppodium P3 in Purmerend! Dus hop, daar gingen we met z'n allen en onze instrumenten met het openbaar vervoer. Onze eerste tegenstanders waren wel aardig, maar na het optreden van de sterke band 'de Ideale Schoonzoons', die een grote fan share hadden meegenomen, moesten we toch even slikken. Zelf hadden we helaas helemaal geen supporters meegenomen maar het lukte ons om de supporters van onze concurrerende band los te laten gaan met een geslaagd optreden! Voor dit optreden werden we beloond met de jury prijs en bekroond tot "beste band van de avond"! Het uitgebreide jury verslag valt <a href="http://www.theclashofthecoverbands.com/show/verslagen/item,671,Voorronde-Regio-West-2013.html"> HIER</a> te lezen.
+De volgende ronde vindt plaats ergens in September in het Paard van Troje in Den Haag (iets dichter bij huis)! Nog (maar) 150 bands te verslaan en we worden de beste band van de Benelux ;)! </p>
         </div>
         
        	<div class="newsitem">
-            <div class="date"><label> 3 </label><span> JUN</span></div>
-        	    <h2>  Laurens neemt deel aan DBSSW!</h2>
-            	<img src="./images/logo.jpg" alt="item pic" title="item title" />
-                <p> Trots als een pauw mag Laurens meedoen aan de beste singer- songwriter van Nederland. Vet Leuk. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. Blaaat. Meer Lorum Ipsum enzo. Blaaat. </p>
+            <div class="date"><label> 25 </label><span> APRIL</span></div>
+        	    <h2>  Optreden  bij windsurf vereniging Plankenkoorts</h2>
+            	<img src="images/albums/Plankenkoorts @ Floratheater_nieuws.jpg" alt="item pic" title="item title" />
+                <p> 25 April traden we op bij een feest genaamd Overstag in het Floratheater in onze mooie thuisstad: Delft. Het Floratheater bleek mooi oud pornotheater compleet met houten bioscoop stoeltjes te zijn. Gelukkig was het vroegere publiek thuis gebleven en stond de zaal nu vol met windsurf dudes en chicks van de delfste studentenvereniging Plankenkoorts. Het publiek was al snel in beweging gebracht en bij het nog redelijk relaxte nummer 'New Shoes' van Paolo Nutini ontstond er al een mosh-pit. De sfeer zat er goed in en de temperatuur nam snel toe! Na afloop van het optreden werd er hard geroepen om NOG EEN NUMMER! En tsja..wij zijn de slechtsten niet, dus hebben we ze nog een toegift cadeau gedaan. Dj's zoals "dj Pilskabouter" namen het publiek van ons over en wij mengden ons in het publiek om van het geslaagde feestje te genieten. </p>
        </div>
            
     </div> <!-- end div content -->
