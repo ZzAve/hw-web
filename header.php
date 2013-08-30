d40a5c7946998337871f691a39f4919e9c85f0ef
Julius van Dis <vandis.j@gmail.com>
2013-08-31 00:56:04 +0200

    Scripts at the end of page, instead of beginning

--- header.php
+++ header.php
@@ -5,20 +5,24 @@
       lang="nl" xml:lang="nl">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta http-equiv="last-modified" content="2013-07-01" />
+    <meta http-equiv="last-modified" content="2013-08-29" />
     <meta http-equiv="content-language" content="nl" />
    	<meta property="fb:admins" content="100001997083297" />
+    
     <link href="images/shortIcon.jpg" rel="shortcut icon" />
     <link rel="image_src" href="http://www.homemadewater.nl/images/logo.jpg" />
     <link rel="image_src" href="http://www.homemadewater.nl/images/screenshot.jpg" />
     <link rel="image_src" href="" />
+    
     <meta name="description" content="Homemade Water is een frisse pop/rock (cover)band die elke zaal om kan toveren in een feestende bende! Groot, klein, jong of oud? Homewade Water krijgt óók u aan het feesten!"/>
     <meta name="keywords" content="Homemade Water, Delft, band, coverband, pop, rock, studenten, feestband, clash, coverbands, student, Laurens Mensink, Andrea Forzoni, Eline Burger, Moos Meijer, Julius van Dis" />
     <meta name="author" content="Homemade Water" />
     <meta name="publisher" content="Homemade Water" />
     <meta name="copyright" content="Homemade Water" />
     <meta name="Homemade Water" content="Delft band cover coverband Laurens Mensink Eline Burger Moos Meijer Andrea Forzoni Julius van Dis" />
-	<title>Homemade Water</title>
+	
+    <title>Homemade Water</title>
+     
     <!-- fonts -->
     <link href='http://fonts.googleapis.com/css?family=Average+Sans' rel='stylesheet' type='text/css' />
 
@@ -30,42 +34,11 @@
     <link rel="stylesheet" type="text/css" href="style/audio.css" title="style" />
     <link rel="stylesheet" type="text/css" href="style/foto.css" title="style" />
     <link rel="stylesheet" type="text/css" href="style/video.css" title="style" />
-    <link rel="stylesheet" type="text/css" href="style/contact.css" title="style" />    
-    
-    <!--- scripts -->
-    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-	<script type="text/javascript"> <!-- google analytics script -->
-	  (function(i,s,o,g,r,a,m){
-		  i['GoogleAnalyticsObject']=r;
-		  i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
-			a=s.createElement(o), m=s.getElementsByTagName(o)[0];
-			a.async=1;
-			a.src=g;
-			m.parentNode.insertBefore(a,m)
-	  })
-	
-	  (window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-41431442-1', 'homemadewater.nl');
-	  ga('send', 'pageview');
-	
-	</script>   
+    <link rel="stylesheet" type="text/css" href="style/contact.css" title="style" />     
 </head>
 
 <body>
-	<!-- Facebook SDK import function -->
-    <div id="fb-root"></div>
-    <script type="text/javascript" async="async">
-		(function(d, s, id) {
-		   var js, fjs = d.getElementsByTagName(s)[0];
-		   if (d.getElementById(id)) return;
-		   js = d.createElement(s); 
-		   js.id = id;
-		   js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-		   fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-    </script>
+
 
 <div id="wrapper">
     <div id="header">       
