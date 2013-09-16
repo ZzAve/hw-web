dc7d9f41ba51ea05e88b382aa8be353f46264cff
Julius van Dis <vandis.j@gmail.com>
2013-09-16 09:20:59 +0200

    Rewrite php code of agenda,foto and news
    De drie bovengenoemde pagina's heben de mogelijkheid om zowel 1 item te
    laden of een overzicht te tonen. De bepaling daarvan was 'slecht'
    gecodeerd. Nu is het beter:
    - Als allereerst wordt er gekeken wat er verwacht wordt (is er een
    request? zo ja, is deze valide (verwijst hij naar een bestaand item))
    - De titel van de pagina en het plaatje voor facebook wordt geupdate (de
    rel_img meta-tag)
    - Vervolgens wordt de pagina op gebouwd door middel van een simpele
    check: Is er een valide request, ja/nee. Als ja, dan bouw 1 item, als
    nee, bouw overzicht.
    
    Deze opbouw is consistent toegepast op alledrie de pagina's

--- foto.php
+++ foto.php
@@ -1,133 +1,112 @@
-<?php	
-	$counter=1;
-	function putPhotoThumb($current,$aLocation,$aName){
-		global $counter;
-		?>
-        
-		<li id="<?=str_replace("_thumb.jpg","",$current)?>"> 
-		  <a href="<?=$aLocation."/".str_replace("_thumb","",$current)?>" data-lightbox="photo-album" title=""/> 
-          	<img src="<?=$aLocation."/".$current?>"/> <!-- show thumbnail --> 
-          </a>
-          <!-- old image thingies -->
-          <!--<a href="img/image-1.jpg" data-lightbox="image-1" title="My caption">image #1</a>
-		  <!-- alt="<?=$aName?>" title="<?=$aName?>" -- 
-		  <span class="hidden prevImage"><?= $previous?></span>
-		  <span class="hidden nextImage"><?= $next?></span>-->
-	   </li>	
-       <?php
-   }
-   
-   
-   	require_once 'header.php'; 
+<?php
 	require_once 'misc/db_connectread.php';
+	$request=false;
+	$valid_request=false;   
+	
+	if(isset($_GET['album'])){
+		$request=true;
+		$albumid=$_GET['album'];
+		
+		//check if album exists
+		$query = "SELECT * FROM `fotoalbums` WHERE `ID`=$albumid;";
+		$result = mysqli_query($mysql,$query);
+		$row = mysqli_fetch_array($result);
+		
+		if (isset($row['ID']) && $row['ID']!="" ) {
+			// now a special album is requested
+			$valid_request=$row;
+		}
+	}
+
+	//Set a facebook image
+	$fb_img = $valid_request!==false ? $valid_request['Foto'] :"";	
+	
+	//Set a title for the page
+	$pre_title=$valid_request!==false ? $valid_request['Titel']." - " : "";
+	$title= $pre_title."Foto's";   
+   	
+	// Import header
+	require_once 'header.php'; 7
 ?>
 
 <div id="content-bar">  
-      <div id="content">
+      <div id="content">		
+<?php  // There are to options to show:
+       //   1. Show a specific item
+       //   2. Show a part of the newsitems
 
-            <?php 
-			$special=FALSE;
-			if(isset($_GET['album'])){
-				$albumid=$_GET['album'];
-				
-				//check if album exists
-				$query = "SELECT * FROM `fotoalbums` WHERE `ID`=$albumid;";
-				$result = mysqli_query($mysql,$query);
-				$row = mysqli_fetch_array($result);
-				
-				if (isset($row['ID']) ) {
-					// now a special album is requested
-					$special=TRUE;
-					
-					$album_id=$row['ID'];
-					$album_name = $row['Titel'];
-					$album_date = $row['Datum'];
-					$album_place = $row['Locatie'];
-					$album_location = $row['Fotofolder'];
-					$album_descr = $row['Omschrijving'];
-					
-					?>
-					<a href="foto.php"> Terug naar het album overzicht</a>            	
-
-               		<h1> Fotoalbum:  </label> <?= $album_name?> </h1>
-               	 	<div id="sharediv">                
-                        <ul>
-                        
-                            <li class="fblike"> 
-                                <script> 
-									document.write('<fb:like href="http://www.homemadewater.nl/foto.php?album=<?=$album_id?>" width="200" layout="button_count" show_faces="false" send="false"></fb:like>')
-                                </script> 
-                            </li>
-                            <li> 
-                                <a href="#"  onclick="window.open(
-                                'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 
-                                    'facebook-share-dialog', 
-                                    'width=626,height=436'); 
-                                    return false;">
-                                    Deel op Facebook</a>
-                            </li>
-                            <li> <div class="g-plusone" data-annotation="inline" data-width="200"></div></li>
-                            <li> 
-                                <a href="https://twitter.com/share" data-text="Wat een tof fotoalbum van dat optreden van Homemade Water" class="twitter-share-button" data-lang="nl">
-                                    Tweeten
-                                </a>
-                            </li>
-                        </ul>
-                    </div><!-- end 	share div -->
-                    
-                    <h2> Datum: <?= $album_date?> <br /> Plaats: <?= $album_place?> </h2>		
-                    <p class="album_descr"> <?=$album_descr?></p>
-    
-                    <div id="thumblist">
-                        <ul>
-                            <?php //get all photo
-                              $curdir=getcwd();
-                              chdir($album_location);
-                              $photolist = glob("*_thumb.jpg");
-                              natsort($photolist);
-                              chdir($curdir);
-                              $firstdone=false;
-							  $prevPhoto = "";
-							  $curPhoto = "";
-							  $nextPhoto = "";
-							  foreach($photolist as $photo){
-								  $prevPhoto = $curPhoto;
-								  $curPhoto = $nextPhoto;
-								  $nextPhoto = $photo;
-                                  if (!$firstdone) {
-									  $firstdone=true;
-								  } else {
-									  putPhotoThumb($curPhoto,$album_location,$album_name);
-								  }
-                              }
-
-                           putPhotoThumb($nextPhoto,$album_location,$album_name);
-						   ?>
-                        </ul>	
-                    </div>
-                    
-                    <div class="fb_comment">
-                        <script>
-                            document.write('<fb:comments href="http://www.homemadewater.nl//foto.php?album=<?=$album_id?>" colorscheme="dark" width="600"></fb:comments>')
-                        </script>
-                    </div>
-					
-					<?php
+	 if($valid_request!==false){
+             // an album is requested
+      	     $album_id=$row['ID'];
+      	     $album_name = $row['Titel'];
+      	     $album_date = $row['Datum'];
+      	     $album_place = $row['Locatie'];
+      	     $album_location = $row['Fotofolder'];
+      	     $album_descr = $row['Omschrijving'];
+?>
+            <a href="foto.php"> Terug naar het album overzicht</a>            	
+            <h1> Fotoalbum:  <label><?= $album_name?></label> </h1>
+            <div id="sharediv">                
+                <ul>
+                    <li class="fblike"> 
+                        <script> 
+                            //<![CDATA[
+                            document.write('<fb:like href="http://www.homemadewater.nl/foto.php?album=<?=$album_id?>" width="200" layout="button_count" show_faces="false" send="false"></fb:like>');
+                            //]]>
+                        </script> 
+                    </li>
+                    <li> 
+                        <a href="#"  onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;">
+                            Deel op Facebook</a>
+                    </li>
+                    <li> <div class="g-plusone" data-annotation="inline" data-width="200"></div></li>
+                    <li> 
+                        <a href="https://twitter.com/share" data-text="Wat een tof fotoalbum van dat optreden van Homemade Water" class="twitter-share-button" data-lang="nl">Tweeten</a>
+                    </li>
+                </ul>
+            </div><!-- end 	share div -->
+			
+            <h2> Datum: <?= $album_date ?></h2>
+            <h2> Plaats: <?= $album_place ?> </h2>		
+            <p class="album_descr"> <?=$album_descr?></p>
+	
+            <div id="thumblist">
+              <ul>
+<?php		    //get all photos
+				$curdir=getcwd();
+				chdir($album_location);
+				$photolist = glob("*_thumb.jpg");
+				natsort($photolist);
+				chdir($curdir);
+				foreach($photolist as $photo){
+				  putPhotoThumb($photo,$album_location,$album_name);
 				}
-			} // end check for special album
+?>
+              </ul>	
+            </div>
 			
+            <div class="fb_comment">
+              <script>
+                //<![CDATA[
+                document.write('<fb:comments href="http://www.homemadewater.nl//foto.php?album=<?=$album_id?>" colorscheme="dark" width="600"></fb:comments>')
+                //]]>
+              </script>
+            </div>
 			
+<?php // END OF SHOWING ONE ALBUM ! ! 
+	 } else { 
+		    // the overview is requested
 			
-			if (!$special){
-				//no special album is requested, so give all.	
-			
-				?>
-                
+			if($request){//show error (through an javascript alert?)
+?>				<script> 
+					setTimeout(function(){alert('Helaas is het door u opgegeven nieuwsbericht niet meer beschikbaar of heeft het nooit bestaan.\n\nDesalniettemin hebben wij voor u de meest recente nieuwsbericht voor u op een rijtje gezet.');},800);
+                </script>
+<?php		}
+?>
                 <h1> Fotoalbums </h1>
                 <h2> Om een beeld te krijgen van een feestje met Homemade Water! </h2>
-                
-                <ul id="album-list">
-					<?php
+                <ul id="album-list">                
+<?php
                     // Show photo-albums in an unordered list (<ul> ... </ul>)
 					$query= "SELECT * FROM `fotoalbums`  WHERE 1 ORDER BY `Datum` DESC;";
 					$result = mysqli_query($mysql,$query);
@@ -139,57 +118,59 @@
 						$album_place = $row['Locatie'];
 						$album_location =  $row['Fotofolder'];
 						$album_thumb = $row['Thumbnail'];
-						?>
-                      	<li> <a href="foto.php?album=<?=$album_id?>"> 
-                        		<img src="<?=$album_thumb?>" alt="<?=$album_name?>" title="<?=$album_name?>" />
-                             </a> 
-							 <label> <?= $album_date?> </label>	<?=$album_name?> <span> <?=$album_place?> </span>
+?>
+                      	<li> 
+                          <a href="foto.php?album=<?=$album_id?>"> 
+                            <img src="<?=$album_thumb?>" alt="<?=$album_name?>" title="<?=$album_name?>" />
+                          </a> 
+                          <label> <?= $album_date?> </label>	<?=$album_name?> <span> <?=$album_place?> </span>
                         </li>
-                    <?php
-                    } 
-				?>
+<?php             } 
+?>
 				</ul> <!--  end thumbnail list of photo albums -->
-                <?php
-			} // end if else (show ONE albums, or albumlist)
-			?>
-            
-
-     </div>
-     
+                
+<?php  } // end if else (show ONE albums, or albumlist)
+?>
+     </div> <!-- end content div -->	   
      <div id="sidebar-left"></div>
      <div id="sidebar-right"></div>   
 </div> <!-- end content-bar -->
-<!--
-<div id="showPhoto" class="hidden"> <!-- div that can be used to "pop up" --
-    <div id="photosite">
-       	<img class="show" src="" alt="De gevraagde foto is helaas niet beschikbaar, door een fout op de server. Deze fout wordt z.s.m. verholpen. Ons excuses voor het ongemak" title="Awesome foto" />
-		<span class="hidden"> Laden . . . </span>
-        <div>
-        	<div id="nextPhoto"> <a href="#">Volgende foto</a></div>
-            <div id="previousPhoto"><a href="#">Vorige foto </a></div> 
-            <div id="closePhotoAlbum" ><img src="images/close_button.png" alt="Close" title="Close" /></div>
-		</div>
-   </div>
-</div> 
--->
+
 <?php require_once 'footer.php'; ?>
 
-<!-- page specific scripts -->
-<script type="text/javascript" src="js/photoalbum.js"></script>
-<script type="text/javascript" src="js/lightbox-2.6.min.js"></script>
-<!-- google plus script -->
-<script type="text/javascript">
-  window.___gcfg = {lang: 'nl'};
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script>
-
-<!-- twitter script -->
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
+<!-- ||| page specific scripts ||| -->
+	<!-- javascript for the photoalbums -->
+	<script type="text/javascript" src="js/photoalbum.js"></script>
+    
+    <!-- lightbox -->
+    <script type="text/javascript" src="js/lightbox-2.6.min.js"></script>
+    
+    <!-- google plus script -->
+    <script type="text/javascript">
+      window.___gcfg = {lang: 'nl'};
+      (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      })();
+    </script>
+    
+    <!-- twitter script -->
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<!-- ||| end page specific scripts ||| -->	
 </body>
 </html>
 
+<?php	
+	$counter=1;
+	function putPhotoThumb($current,$aLocation,$aName){
+		global $counter;
+?>
+                <li id="<?=str_replace("_thumb.jpg","",$current)?>"> 
+                    <a href="<?=$aLocation."/".str_replace("_thumb","",$current)?>" data-lightbox="photo-album" title="<?=$aName?>"/> 
+                      <img src="<?=$aLocation."/".$current?>"/> <!-- show thumbnail --> 
+                    </a>
+                </li>	
+<?php
+   }
+?>
--- nieuws.php
+++ nieuws.php
@@ -1,62 +1,86 @@
-<?php 
-	  require_once 'header.php';
-	  require_once('misc/db_connectread.php');
-	  setlocale(LC_TIME, 'Dutch'); 
+<?php
+	require_once('misc/db_connectread.php');
+	$request = false;
+	$valid_request = false;
+	
+	// Check what to represent
+ 	if(isset($_GET['item'])){
+		$request=true;
+		$id=intval($_GET['item']);
+		$query = "SELECT * FROM  `nieuwsitems` WHERE  `ID` = $id";
+		$result = mysqli_query($mysql,$query);
+		$row = mysqli_fetch_array($result);
+		
+		if ($row != NULL && $row['ID']!= "" ) {
+			$valid_request=$row;
+		}
+	}
+    //Set a title for the page
+	$pre_title = $valid_request!==false ? $valid_request['Titel']." - " : "";
+    $title = $pre_title."Nieuws";
+	
+	//Set a facebook image
+	$fb_img = $valid_request!==false ? $valid_request['Foto'] :"";
+	
+	//Import header
+    require_once 'header.php';  
+    setlocale(LC_TIME, 'Dutch'); 
 ?>
 
 <div id="content-bar">
     <div id="content">
 		    
-    	<?php // check whether 1 newsitem is requested or something else
-			$special=false; //assume visitor does not want something other than the standard page
+<?php	// There are to options to show:
+		// 	1. Show a specific item
+		//  2. Show a part of the newsitems
 
-			if(isset($_GET['id'])){
-				$id=intval(str_replace('item','',$_GET['id']));
-				$query = "SELECT * FROM  `nieuwsitems` WHERE  `ID` = $id";
-				$result = mysqli_query($mysql,$query);
-				$row = mysqli_fetch_array($result);
-				if ($row != NULL && $row['ID']!= "" ) {
-					$prev_news = strstr(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "" ,"?startat=");
-					?> 
-                    <p> 
-                    	<a href="nieuws.php<?= $prev_news ? $prev_news : "" ?>">Ga terug naar het nieuwsoverzicht</a>
-                    </p>
-                    <?php
-					// create only 1 newsitem
-					$special=true;
-					popnewsitem($row);
-					?>
-                    <p> <a href="nieuws.php<?= $prev_news ? $prev_news : "" ?>"> Ga terug naar het nieuwsoverzicht</a></p>
-                    <?php
-				}
-			}
-			
-			if(!$special){
-				  $query = "SELECT `ID` FROM `nieuwsitems` ORDER BY `ID` DESC LIMIT 1;";
-				  $result = mysqli_query($mysql,$query);
-				  $ans = mysqli_fetch_array($result);
-				  $start = $ans['ID'];
-				  $top = $start;
-				  if(isset($_GET['startat']) && intval($_GET['startat'])>=10 && intval($_GET['startat'])<=$top ){
-				 	// give the five newsitems starting from number 'startat'
-					$start=intval($_GET['startat']);
-		  		  }
-				  ?>
-				<h1> Het laatste nieuws!</h1> 
-                <h2> Om ons op de voet te volgen, verwijzen we u door naar onze <a href="http://www.facebook.com/HomemadeWater">facebook pagina</a></h2>  
-				
-                 <?php
-                  include 'misc/morenews.php';
-                  $query = "SELECT * FROM `nieuwsitems` WHERE `ID`<=$start ORDER BY `Datum` DESC LIMIT 5;";
-                  $result = mysqli_query($mysql,$query);
-                  while( $row = mysqli_fetch_array($result) )  {
-                      popnewsitem($row);
-                  }
-                       		
-                  include 'misc/morenews.php';
-	  	    } // end check for special loop
-	   ?>       
-           
+	if($valid_request!==false){
+			// Show one item
+			$prev_news = strstr(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "" ,"?startat=");
+?> 
+            <p> 
+            	<a href="nieuws.php<?= $prev_news ? $prev_news : "" ?>">Ga terug naar het nieuwsoverzicht</a>
+            </p>
+<?php
+			popnewsitem($valid_request);
+?>
+            <p> <a href="nieuws.php<?= $prev_news ? $prev_news : "" ?>"> Ga terug naar het nieuwsoverzicht</a></p>
+<?php
+
+	} else {
+		
+		// Show 'all' the newsitems
+		if($request){//show error (through an javascript alert?)
+?>
+			<script> 
+				setTimeout(function(){alert('Helaas is het door u opgegeven nieuwsbericht niet meer beschikbaar of heeft het nooit bestaan.\n\nDesalniettemin hebben wij voor u de meest recente nieuwsbericht voor u op een rijtje gezet.');},800);
+			</script>
+<?php
+		}
+		
+		$query = "SELECT `ID` FROM `nieuwsitems` ORDER BY `ID` DESC LIMIT 1;";
+		$result = mysqli_query($mysql,$query);
+		$ans = mysqli_fetch_array($result);
+		$start = $ans['ID'];
+		$top = $start;
+		if(isset($_GET['startat']) && intval($_GET['startat'])>=10 && intval($_GET['startat'])<=$top ){
+	 	  // give the five newsitems starting from number 'startat'
+		  $start=intval($_GET['startat']);
+		}
+?>
+        <h1> Het laatste nieuws!</h1> 
+        <h2> Om ons op de voet te volgen, verwijzen we u door naar onze <a href="http://www.facebook.com/HomemadeWater">facebook pagina</a></h2>  
+<?php
+            morenews($start,$top);
+            $query = "SELECT * FROM `nieuwsitems` WHERE `ID`<=$start ORDER BY `Datum` DESC LIMIT 5;";
+            $result = mysqli_query($mysql,$query);
+            while( $row = mysqli_fetch_array($result) )  {
+              popnewsitem($row);
+            }                 		
+            
+			morenews($start,$top);
+		}	
+?>         
     </div> <!-- end div content -->
 	<div id="sidebar-left"></div>
     <div id="sidebar-right"></div>
@@ -69,48 +93,101 @@
 
 
 <?php 
-	/* function POPNEWSITEM creates a <div> entry for one single newsitem. It checks whether multiple newsitem need to be posted (by means of variable $special, and if so it only posts short versions of the items.
-	//	input:
-	//		$db_entry   a database entry with the newsitem, coming from the 'newsitems' table of the 'juliuqb30_hw' database.
-	//  output:
-	// 		$ one <div> with class 'newsitem' (and 'single' in case only one newsitem is requested.
-	//  returns nothing
-	*/
-	function popnewsitem($db_entry){
-		global $special;
-		$date = explode("-",$db_entry['Datum']);
-		?> 
-		<div class="newsitem <?= $special ? "single" : ""?>" id="<?="item" . $db_entry['ID']?>">
-		   <div class="date">
-			  <label> <?= array_pop($date)?> </label>
-			  <span> <?= strtoupper(strftime("%b",mktime(0, 0, 0, array_pop($date) ) ) )?> </span>
-		   </div>
-           
-           <?php // make both the title and image a link, such that if clicked, a screen opens with only that newsitem?>
-		   <a href="<?= "nieuws.php?id=item".$db_entry['ID']?> "><h3>  <?= $db_entry['Titel']?> </h3></a>
-		   <a href="<?= "nieuws.php?id=item".$db_entry['ID']?>"> <img src="<?=$db_entry['Foto']?>" alt="<?=$db_entry['Alt_foto']?>" title="<?=$db_entry['Alt_foto']?>"/></a>
-		   <?php
-		   	 //Ensure what message to post. 
-			 //	 A short one (in case of multiple items) 
-			 //  or the whole text (in case a single newsitem is requested).
-		   	 $msg=$db_entry['Bericht'];
-			 if (!$special){ 
-			 	// a single item is not requested, thus do not show entire text!
-           	 	$msg=strstr($msg,"</p>",TRUE)."</p>";
-			 }
-		  ?>
-             
- 		  <?=$msg?>
-         
-          <?php 
-            if(!$special){ 
-                ?> 
-                <p><a href="<?= "nieuws.php?id=item" .$db_entry['ID']?>"> Lees meer...</a> </p>
-                <?php 
-            } 
-          ?>
-           
-	    </div>	
-        <?php
-	}
+/* function POPNEWSITEM creates a <div> entry for one single newsitem. It checks whether multiple newsitem need to be posted (by means of variable $special, and if so it only posts short versions of the items.
+//	input:
+//		$db_entry   a database entry with the newsitem, coming from the 'newsitems' table of the 'juliuqb30_hw' database.
+//  output:
+// 		$ one <div> with class 'newsitem' (and 'single' in case only one newsitem is requested.
+//  returns nothing
+*/
+function popnewsitem($db_entry){
+	global $valid_request;
+	$date = explode("-",$db_entry['Datum']);
+?> 
+    <div class="newsitem <?= $valid_request!==false ? "single" : ""?>" id="<?="item" . $db_entry['ID']?>">
+      <div class="date">
+        <label> <?= array_pop($date)?> </label>
+        <span> <?= strtoupper(strftime("%b",mktime(0, 0, 0, array_pop($date) ) ) )?> </span>
+      </div>
+
+<?php  // make both the title and image a link, such that if clicked, a screen opens with only that newsitem
+	   if($valid_request===false){
+?>
+      <a href="<?= "nieuws.php?item=".$db_entry['ID']?> "><h3><?= $db_entry['Titel']?> </h3></a>
+      <a href="<?= "nieuws.php?item=".$db_entry['ID']?>"> <img src="<?=$db_entry['Foto']?>" alt="<?=$db_entry['Alt_foto']?>" title="<?=$db_entry['Alt_foto']?>"/></a>
+<?php
+	   } else {
+?>
+      <h3><?= $db_entry['Titel']?> </h3>
+      <img src="<?=$db_entry['Foto']?>" alt="<?=$db_entry['Alt_foto']?>" title="<?=$db_entry['Alt_foto']?>"/>
+<?php	
+	   }
+
+		 //Ensure what message to post. 
+		 //	 A short one (in case of multiple items) 
+		 //  or the whole text (in case a single newsitem is requested).
+		 $msg=$db_entry['Bericht'];
+		 if ($valid_request===false){ 
+			// a single item is not requested, thus do not show entire text!
+			$msg=strstr($msg,"</p>",TRUE)."</p>";
+		 }
+?>
+      <?=$msg ?>
+<?php
+
+		if($valid_request===false){ 
+?> 
+      <p><a href="<?= "nieuws.php?item=" .$db_entry['ID']?>"> Lees meer...</a> </p>
+<?php 
+		} 
+?>
+    </div>	
+<?php
+} //end function POPNEWSITEM
+
+/*
+Function MORENEWS puts in a div, with the text 'vorige pagina' and 'volgende pagina'. It dynamically determines what the next or previous page is based on what is currently shown.
+*
+Input:
+  $start 	this variable refers to the ID of the first newsitem showing on the current page
+  $top 		this variable refers to the item with highest ID in the database (and very likely to be the most recent one)
+*
+Output:
+  one <div> of class 'morenews', which holds two <span> elements, for both the previous and next news page respectively.
+*/
+function morenews($start,$top){
+?>
+        <!-- Allow visitor to see more news -->
+        <div class="morenews">
+            <span>
+<?php 
+    if($start<$top){
+      // show previous with link
+?>
+              <a href="<?= "nieuws.php?startat=".($start+5)?>" >Vorige pagina </a>
+<?php
+    } else { // show without link
+?>
+                  Vorige pagina
+<?php 
+    }
+?>
+            </span>					                
+            <span>
+    <?php   
+        if($start>10+4){
+          //show next with link
+?>
+              <a href="<?= "nieuws.php?startat=".($start-5)?>" >Volgende pagina </a>
+<?php
+        } else{ //show without link 
+?> 
+                Volgende pagina
+<?php 
+		} 
+?>
+            </span>
+        </div>  
+<?php     	
+} // end function MORENEWS
 ?>
