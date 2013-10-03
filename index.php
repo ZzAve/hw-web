fc5188da6d6c83d09a450a571f6bda880280f37e
Julius van Dis <vandis.j@gmail.com>
2013-10-03 21:17:51 +0200

    Added Clash of The Coverbands
    Added a <div> element on the homepage about the Clash of the Coverbands.
    It ain't really pretty, but it draws attention....

--- index.php
+++ index.php
@@ -5,9 +5,6 @@
 
          <p> Homemade Water is een frisse pop-rock(cover)band die naam begint te krijgen in Delft en de rest van Nederland. Zij brengt een strakke en gevarie&euml;rde set bekendere rock- en popnummers en enkele eigen nummers. Met deze set zijn al vele zalen, café's en sociëteiten op de kop gezet waarbij de temperatuur regelmatig boven het kookpunt is uitgestegen! </p>    
 
-         
-       <!--  <p> Op onze site kunt u terecht voor al het moois dat Homemade Water de wereld te bieden heeft. Zo kunt u ons laatste nieuws bekijken, onze unieke sound beluisteren, maar u kunt een aantal optreden herleven met uniek beeld- en videomateriaal. Mocht u contact willen opnemen, schroom dan vooral niet.</p>-->
-
          <p> Homemade Water bestaat uit vijf technische studenten met passie voor muziek:</p>
          <ul>
          	<li>Zanger Laurens Mensink</li>
@@ -18,7 +15,18 @@
          </ul>
 		
          <p> Nieuwsgierig geworden? Check onze <a href="./audio.php">media</a> en <a href="./nieuws.php">laatste nieuws</a>! <br />. . . . en vergeet niet om ons te liken op facebook! (zie het icoontje onderaan deze pagina)</p>       
-    </div>
+         <div class="attention">
+             <a href="images/Clash_kwartfinale.jpg"><img src="images/Clash_kwartfinale.jpg" alt="De line up voor de kwartfinale van de Clash of The Coverbands" /></a> 
+             <h3> Jouw hulp is geboden! </h3>
+             
+             <p>Homemade Water doet mee aan de Clash of the Coverbands, en strijdt mee om de felbegeerde titel van 'beste coverband van de Benelux'. Inmiddels zitten we in de kwartfinale van de regio West.</p>
+            
+             <p> In deze ronde strijden we tegen 6 andere covenbandjes uit de randstad. Helaas vallen en 3 van de 7 bands af die avond en daar hopen wij niet tussen te zitten. Op de poster is te zien welke andere bands er nog meer spelen die avond. Het belooft in ieder geval een geweldige avond te worden!</p>
+            
+             <p>Maar om door naar de volgende ronde te kunnen hebben we JOUW hulp nodig. Elke bezoeker krijgt bij binnenkomst een stembiljet, en dus zo de mogelijkheid om te stemmen op de band die hij of zijn het beste vond. Kom ook en stem op ons!</p>
+             <p> Kaarten zijn <a href= "http://www.artistsandbandsbv.nl/a-29667520/the-clash-of-the-cover-bands/1-november-2013-total-music-schiedam/" >hier</a> verkrijgbaar. </p>
+        </div>
+    </div> <!-- end div content -->
 	<div id="sidebar-left"></div>
     <div id="sidebar-right"></div>
     
